bb2f3a955b5e2f023a8154c47fb69d836ac7e99a
Harsha-Vardhan-S <harshavardhans.cloud@gmail.com>
2023-10-10 14:43:04 +0530

    Coding exercise 2

--- helloworld.py
+++ helloworld.py
@@ -106,6 +106,6 @@ print(("-" * 20) + "End of Topic" + ("-" * 20) + "\n")  # --------------------En
 # ------------------------------------------------------------------------------------------------------------
 # Coding Exercise 2: Printing the result of a calculation
 # Write a program to print the answer to the calculation 6 times 7
-print(6 * 7)
+print(6 * 7)        # 42
 
 print(("-" * 20) + "End of Topic" + ("-" * 20) + "\n")  # --------------------End of Topic--------------------
