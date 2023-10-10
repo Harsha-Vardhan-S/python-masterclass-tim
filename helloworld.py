5b6a0dc7e847ee2b9716cb6df7ba32d542cb15a6
Harsha-Vardhan-S <harshavardhans.cloud@gmail.com>
2023-10-10 14:42:42 +0530

    Coding exercise 2

--- helloworld.py
+++ helloworld.py
@@ -102,3 +102,10 @@ print(("-" * 20) + "End of Topic" + ("-" * 20) + "\n")  # --------------------En
 print("My hovercraft is full of eels")      # My hovercraft is full of eels
 
 print(("-" * 20) + "End of Topic" + ("-" * 20) + "\n")  # --------------------End of Topic--------------------
+
+# ------------------------------------------------------------------------------------------------------------
+# Coding Exercise 2: Printing the result of a calculation
+# Write a program to print the answer to the calculation 6 times 7
+print(6 * 7)
+
+print(("-" * 20) + "End of Topic" + ("-" * 20) + "\n")  # --------------------End of Topic--------------------
